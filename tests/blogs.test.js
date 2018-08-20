b41a5ca8917f619726df329e297b6c5f848b0f21
Oleksandr Pronin <oleksandr.pronin@adviqo.com>
2018-08-20 20:20:07 +0200

    Section 5. 110. Nested Describes for Structure.

--- tests/blogs.test.js
+++ tests/blogs.test.js
@@ -11,10 +11,14 @@ afterEach(async () => {
     await page.close();
 });
 
-test('when logged in, can see blog create form', async () => {
-    await page.login();
-    await page.click('a.btn-floating');
+describe('When logged in', async () => {
+    beforeEach(async () => {
+        await page.login();
+        await page.click('a.btn-floating');
+    });
 
-    const label = await page.getContentsOf('form label');
-    expect(label).toEqual('Blog Title');
+    test('can see blog create form', async () => {
+        const label = await page.getContentsOf('form label');
+        expect(label).toEqual('Blog Title');
+    });
 });
