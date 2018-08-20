bcbf8393ee9bdaf908473d66689f25de9d1baf73
Oleksandr Pronin <oleksandr.pronin@adviqo.com>
2018-08-20 21:23:10 +0200

    Section 5. 117. Asserting Page Response.

---
+++ TemporaryFileToCreateBlogPost.js
@@ -0,0 +1,12 @@
+
+
+() => {
+    fetch('/api/blogs', {
+        method: 'POST',
+        credentials: 'same-origin',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({ title: 'My Title', content: 'My Content' })
+    });
+}
