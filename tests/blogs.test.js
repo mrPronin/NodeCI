8512d82fcd0905da318d5246b0c63f741ea88aa0
Oleksandr Pronin <oleksandr.pronin@adviqo.com>
2018-08-21 17:20:42 +0200

    Section 5. 120. Super Advanced Test Helpers. 02.

--- tests/blogs.test.js
+++ tests/blogs.test.js
@@ -61,6 +61,30 @@ describe('When logged in', async () => {
 });
 
 describe('User is not logged in', async () => {
+    const actions = [
+        {
+            method: 'get',
+            path: '/api/blogs'
+        },
+        {
+            method: 'post',
+            path: '/api/blogs',
+            data: {
+                title: 'T',
+                content: 'C'
+            }
+        }
+    ];
+
+    test('Blog related actions are prohibited', async () => {
+        const results = await page.execRequests(actions);
+
+        for (let result of results) {
+            expect(result).toEqual({ error: 'You must log in!' })
+        }
+    });
+
+    /*
     test('User cannot create blog posts', async () => {
         const result = await page.post('/api/blogs', { title: 'T', content: 'C' });
 
@@ -72,4 +96,5 @@ describe('User is not logged in', async () => {
 
         expect(result).toEqual({ error: 'You must log in!' });
     });
+    */
 });
